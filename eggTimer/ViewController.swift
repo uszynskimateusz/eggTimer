81b7801cf66fcc245307bce8036b0930c7704e44
uszynskimateusz <uszynskimateusz@gmail.com>
2020-08-28 16:49:52 +0200

    timer added

--- eggTimer/ViewController.swift
+++ eggTimer/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let eggTime = ["soft": 3, "medim": 5, "hard": 7]
+    
+    var timeRemaining = 10
+    var timer = Timer()
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
-
+    @IBAction func hardnessPressed(_ sender: UIButton) {
+        
+        titleLabel.text = "Egg Cooking Calculator"
+        
+        let hardness = sender.currentTitle! //eggTime
+        
+        timeRemaining = eggTime[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        //example functionality
+        if timeRemaining > 0 {
+            print(timeRemaining)
+            timeRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+        }
+    }
 }
 
