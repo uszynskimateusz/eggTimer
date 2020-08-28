725f862e0bff7553dedb586fc47f7166012a87ca
uszynskimateusz <uszynskimateusz@gmail.com>
2020-08-28 13:24:20 +0200

    Initial Commit

---
+++ eggTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eggTimer
+//
+//  Created by Mateusz Uszyński on 28/08/2020.
+//  Copyright © 2020 Mateusz Uszyński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
